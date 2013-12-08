c7bd7fb19dc574c41861b7ac229579c75a30a34a
silicon <silicon@nachuantech.com>
2013-12-08 11:31:01 +0800

    netease bottom  menu source code

---
+++ NetEaseNews/gen/my/project/neteasenews/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package my.project.neteasenews;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ NetEaseNews/src/my/project/neteasenews/TabFollowActivity.java
@@ -0,0 +1,14 @@
+package my.project.neteasenews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabFollowActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_follow);
+	}
+}
